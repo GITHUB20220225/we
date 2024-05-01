f2c76cb31b1691cfc5ce4a4c747bed0ad65c5091
조명균 <100388813+GITHUB20220225@users.noreply.github.com>
2024-05-01 14:41:37 +0900

    Add files via upload

---
+++ dar.js
@@ -0,0 +1,85 @@
+function hidebutton1()  {
+    const row = document.getElementById('hideshow1');
+    row.style.display = 'none';
+}
+  
+function showbutton1()  {
+    const row = document.getElementById('hideshow1');
+    row.style.display = '';
+}
+function hidebutton2()  {
+  const row = document.getElementById('hideshow2');
+  row.style.display = 'none';
+}
+
+function showbutton2()  {
+  const row = document.getElementById('hideshow2');
+  row.style.display = '';
+}
+function hidebutton3()  {
+  const row = document.getElementById('hideshow3');
+  row.style.display = 'none';
+}
+
+function showbutton3()  {
+  const row = document.getElementById('hideshow3');
+  row.style.display = '';
+}
+function hidebutton4()  {
+  const row = document.getElementById('hideshow4');
+  row.style.display = 'none';
+}
+
+function showbutton4()  {
+  const row = document.getElementById('hideshow4');
+  row.style.display = '';
+}
+function hidebutton5()  {
+  const row = document.getElementById('hideshow5');
+  row.style.display = 'none';
+}
+
+function showbutton5()  {
+  const row = document.getElementById('hideshow5');
+  row.style.display = '';
+}
+
+function hidebutton6()  {
+  const row = document.getElementById('hideshow6');
+  row.style.display = 'none';
+}
+
+function showbutton6()  {
+  const row = document.getElementById('hideshow6');
+  row.style.display = '';
+}
+
+function hidebutton7()  {
+  const row = document.getElementById('hideshow7');
+  row.style.display = 'none';
+}
+
+function showbutton7()  {
+  const row = document.getElementById('hideshow7');
+  row.style.display = '';
+}
+
+function hidebutton8()  {
+  const row = document.getElementById('hideshow8');
+  row.style.display = 'none';
+}
+
+function showbutton8()  {
+  const row = document.getElementById('hideshow8');
+  row.style.display = '';
+}
+
+function hidebutton9()  {
+  const row = document.getElementById('hideshow9');
+  row.style.display = 'none';
+}
+
+function showbutton9()  {
+  const row = document.getElementById('hideshow9');
+  row.style.display = '';
+}
